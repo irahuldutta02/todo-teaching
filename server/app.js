822765cc48af03d15de65a4c18b6c187f616824b
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2025-04-02 23:05:42 +0530

    chore: add middleware for body parsing and CORS support

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.text()); // for parsing text/plain
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
+
+// TODO: database connection
 
 app.get("/", (req, res) => {
   return res.status(200).json({
