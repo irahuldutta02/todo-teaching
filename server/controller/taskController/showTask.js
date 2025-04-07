86ac0f4847e2c18c440187eba05abcd8a1756fa9
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2025-04-08 00:24:20 +0530

    chore : Formate the code

--- server/controller/taskController/showTask.js
+++ server/controller/taskController/showTask.js
@@ -1,31 +1,28 @@
-import { Task } from "../../model/task.model.js"
+import { Task } from "../../model/task.model.js";
 
 export const showTask = async (req, res) => {
-    try {
-       const AllTask = await Task.find();
-        if (!AllTask) {
-           return  res.status(200).json({
-                status: 200,
-                error: false,
-                message: "No Task Yet",
-
-            })
-        }
-        return res.status(200).json({
-            status: 200,
-            error: false,
-            message: "All Task Show successfully",
-            data: AllTask
-        })
-
-    } catch (error) {
-        // Handle errors
-        console.error(error);
-        return res.status(500).json({
-            status: 500,
-            error: true,
-            message: error.message
-        })
-
+  try {
+    const AllTask = await Task.find();
+    if (!AllTask) {
+      return res.status(200).json({
+        status: 200,
+        error: false,
+        message: "No Task Yet",
+      });
     }
-}
+    return res.status(200).json({
+      status: 200,
+      error: false,
+      message: "All Task Show successfully",
+      data: AllTask,
+    });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    return res.status(500).json({
+      status: 500,
+      error: true,
+      message: error.message,
+    });
+  }
+};
