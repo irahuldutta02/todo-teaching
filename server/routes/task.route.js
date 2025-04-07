5b2dbe522ea95994eb5e840312d5dc1c534f1465
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2025-04-08 01:33:00 +0530

    feat: update task routes to include single task retrieval and improve route comments

--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,19 +1,23 @@
 import express from "express";
 import { createTask } from "../controller/taskController/createTask.js";
+import { deleteTask } from "../controller/taskController/deleteTask.js";
+import { getSingleTask } from "../controller/taskController/getSingleTask.js";
 import { getTasks } from "../controller/taskController/getTasks.js";
 import { updatedTask } from "../controller/taskController/updatedTask.js";
-import { deleteTask } from "../controller/taskController/deleteTask.js";
 
 const taskRoutes = express.Router();
 
-// GET: /api/tasks/tasks_get
+// GET: /api/tasks/tasks_get ✅
 taskRoutes.get("/tasks_get", getTasks);
 
-// POST: /api/tasks/task_create
-taskRoutes.post("/task_create", createTask);
+// GET: /api/tasks/single_task_get ✅
+taskRoutes.get("/single_task_get/:id", getSingleTask);
 
-// PUT: /api/tasks/task_update
-taskRoutes.put("/task_update", updatedTask);
+// PUT: /api/tasks/task_update ✅
+taskRoutes.put("/task_update/:id", updatedTask);
+
+// POST: /api/tasks/task_create ✅
+taskRoutes.post("/task_create", createTask);
 
 // DELETE: /api/tasks/task_delete
 taskRoutes.delete("/task_delete", deleteTask);
